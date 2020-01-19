3f08835e3a17d11ac54c9949885d6f1f0cae5221
jumah safarty <safartyjumah@gmail.com>
2020-01-19 02:36:14 +0200

    Creating the theme

---
+++ NeumorphismUI/NeumorphismUI/ValueConverters/WidthHeightToRectangleConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NeumorphismUI
+{
+    /// <summary>
+    /// A converter that takes in a boolean and returns a <see cref="Visibility"/>
+    /// </summary>
+    public class WidthHeightToRectangleConverter : BaseValueConverter<WidthHeightToRectangleConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var control = value as Grid;
+
+            if (control == null)
+                return null;
+
+            return new System.Windows.Rect()
+            {
+                Height = control.Height,
+                Width = control.Width,
+                X = 6,
+                Y = 6
+
+            };
+
+            if (parameter == null)
+                return (bool)value ? Visibility.Hidden : Visibility.Visible;
+            else
+                return (bool)value ? Visibility.Visible : Visibility.Hidden;
+
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
